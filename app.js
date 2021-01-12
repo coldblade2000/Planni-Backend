a4dea4892b94800cb33cc68005c434cb8a8b3fa3
coldblade2000 <diegogranada7@gmail.com>
2021-01-11 19:13:02 -0500

    change

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import {BACKEND_PASSWORD, LOGIN_INFO, SESSION_SECRET} from "./secrets";
+import {BACKEND_PASSWORD, SESSION_SECRET} from "./secrets";
 import * as mongoose from "mongoose";
 import {MONGODB_ADDRESS} from "./constants";
 
@@ -31,9 +31,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-mongoose.connect(`mongodb://${LOGIN_INFO}${MONGODB_ADDRESS}`, {
+mongoose.connect(`mongodb://${MONGODB_ADDRESS}`, {
     user: 'backend',
-    password: BACKEND_PASSWORD,
+    pass: BACKEND_PASSWORD,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
